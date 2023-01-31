d031d2f4bccea64971e4d365c3a633ca750e4822
Timtol13 <94846907+Timtol13@users.noreply.github.com>
2023-02-01 02:00:39 +0300

    Add forms

--- src/components/registration/advance/Advance.tsx
+++ src/components/registration/advance/Advance.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import {useParams} from "react-router";
+import {Player} from "./Forms/Player";
+import styles from './Advance.module.scss'
 
 
 export const Advance = () => {
     const {role} = useParams()
+    const form = () => {
+        switch (role) {
+            case 'Player':
+                return <Player/>
+        }
+    }
     return (
-        <div>
-            {role}
+        <div className={styles.container}>
+            {form()}
         </div>
     )
 }
