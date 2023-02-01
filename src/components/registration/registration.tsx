881e346e4126afaf7398e9d9890cd872ae3a8cf5
Timtol13 <94846907+Timtol13@users.noreply.github.com>
2023-02-01 13:30:31 +0300

    Add forms

--- src/components/registration/registration.tsx
+++ src/components/registration/registration.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import {registrationApi, getUsers} from "../Api/API"
 import {useNavigate} from "react-router";
+import './Registration.modul.scss'
 
 export const Registration = () => {
     const [username, setUsername] = useState('')
@@ -9,24 +10,26 @@ export const Registration = () => {
     let nav = useNavigate()
     const [password, setPassword] = useState('')
     return (
-        <div>
-            <input placeholder={'Введите логин'} id={'username'} onChange={e => setUsername(e.target.value)} />
-            <input placeholder={'Введите e-mail'} id={'email'} onChange={e => setEmail(e.target.value)} />
-            <input placeholder={'Введите пароль'} id={'password'} onChange={e => setPassword(e.target.value)} />
-            <select id={'role'} onChange={e => setRoleA(e.target.value)}>
-                <option value={'Player'}>Игрок</option>
-                <option value={'Agent'}>Агент</option>
-                <option value={'Trainer'}>Тренер</option>
-                <option value={'Parent'}>Родитель</option>
-                <option value={'Club'}>Клуб</option>
-                <option value={'Scout'}>Скаут</option>
-            </select>
-            <button onClick={() => {
-                const data = {username, email, password}
-                console.log(roleA)
-                registrationApi.postRegistration(data).then(() => {return nav(`/advance/${roleA}`)})
-            }
-            }>Зарегистрирвоаться</button>
-        </div>
+        <form>
+            <div>
+                <input placeholder={'Введите логин'} id={'username'} onChange={e => setUsername(e.target.value)} />
+                <input placeholder={'Введите e-mail'} id={'email'} onChange={e => setEmail(e.target.value)} />
+                <input placeholder={'Введите пароль'} id={'password'} onChange={e => setPassword(e.target.value)} />
+                <select id={'role'} onChange={e => setRoleA(e.target.value)}>
+                    <option value={'Player'}>Игрок</option>
+                    <option value={'Agent'}>Агент</option>
+                    <option value={'Trainer'}>Тренер</option>
+                    <option value={'Parent'}>Родитель</option>
+                    <option value={'Club'}>Клуб</option>
+                    <option value={'Scout'}>Скаут</option>
+                </select>
+                <button onClick={() => {
+                    const data = {username, email, password}
+                    console.log(roleA)
+                    registrationApi.postRegistration(data).then(() => {return nav(`/advance/${roleA}`)})
+                }
+                }>Зарегистрирвоаться</button>
+            </div>
+        </form>
     )
 }
