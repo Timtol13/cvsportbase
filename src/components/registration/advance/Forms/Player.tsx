84dc53f0dea4faa7166023f32698ea9e3be99b76
Timtol13 <94846907+Timtol13@users.noreply.github.com>
2023-02-03 02:32:24 +0300

    Add styles

--- src/components/registration/advance/Forms/Player.tsx
+++ src/components/registration/advance/Forms/Player.tsx
@@ -34,19 +34,21 @@ export const Player = () => {
     })
 
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <input {...formik.getFieldProps('first_name')}/>
-            <input {...formik.getFieldProps('second_name')}/>
-            <input {...formik.getFieldProps('patronymic')}/>
-            <input {...formik.getFieldProps('age')}/>
-            <input {...formik.getFieldProps('height')}/>
-            <input {...formik.getFieldProps('weight')}/>
-            <input {...formik.getFieldProps('phone')}/>
-            <input {...formik.getFieldProps('email')}/>
-            <input {...formik.getFieldProps('country')}/>
-            <input {...formik.getFieldProps('city')}/>
-            <input {...formik.getFieldProps('shengen')}/>
-            <button type="submit">Добавить</button>
+        <form onSubmit={formik.handleSubmit} className={styles.form}>
+            <div className={styles.inputs}>
+                <input {...formik.getFieldProps('first_name')} placeholder={'Имя'} />
+                <input {...formik.getFieldProps('second_name')} placeholder={'Фамилия'} />
+                <input {...formik.getFieldProps('patronymic')} placeholder={'Отчество'} />
+                <input {...formik.getFieldProps('age')} placeholder={'Возраст'} />
+                <input {...formik.getFieldProps('height')} placeholder={'Рост'} />
+                <input {...formik.getFieldProps('weight')} placeholder={'Вес'} />
+                <input {...formik.getFieldProps('phone')} placeholder={'Телефон'} />
+                <input {...formik.getFieldProps('email')} placeholder={'E-mail'} />
+                <input {...formik.getFieldProps('country')} placeholder={'Страна'} />
+                <input {...formik.getFieldProps('city')} placeholder={'Город'} />
+                <input {...formik.getFieldProps('shengen')} className={styles.checkbox} type={'checkbox'} /> <i>Наличие шенгена</i>
+                <button type="submit">Добавить</button>
+            </div>
         </form>
     )
 }
