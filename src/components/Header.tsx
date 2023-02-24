4e536112b5e6f226b574b7635ee51781e76b02f0
Timtol13 <94846907+Timtol13@users.noreply.github.com>
2023-02-24 11:56:33 +0300

    Photo taken from DRF to profiles(not working now)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,16 +8,6 @@ import {useDispatch} from "react-redux";
 let user = localStorage.getItem('app-state')
 
 export const Header = () => {
-    const dispatch = useDispatch()
-    const uploadHandler = (e: ChangeEvent<HTMLInputElement>): void => {
-        if (e.target.files && e.target.files.length) {
-            const file = e.target.files[0];
-            let fd = new FormData()
-            fd.append('video', file, file.name)
-            fd.append('user', JSON.parse(!user ? '' : user).auth.me.username)
-            // dispatch(addVideoTC({user: fd ? fd.get('user') : '', video: fd ? fd.get('video') : ''}))
-        }
-    };
     return (
         <div className={styles.header}>
             <div className={styles.logo}>
